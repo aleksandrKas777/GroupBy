5d9983c5af91f466279512035ad3e827e1126f87
aleksandrKas777 <81757942+aleksandrKas777@users.noreply.github.com>
2021-12-11 14:25:59 +0300

    сделал задание

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const groupBy = (arr, key) => {
+    if (arr === undefined) {
+        throw new Error("не передан массив");
+    } else if (key === undefined) {
+        throw new Error("не передан ключ");
+    } else {
+        return arr.reduce((acc, item) => {
+            if (key in item) {
+                (acc[item[key]] = acc[item[key]] || []).push(item);
+            }
+            return acc
+        }, {});
+    }
+}
+
+console.log(groupBy([
+    {id: 1, universe: "marvel", name: "Spider Man"},
+    {id: 2, universe: "marvel", name: "Iron Man"},
+    {id: 3, universe: "dc", name: "Aqua Man"},
+    {id: 4, universe: "dc", name: "Bat Man"},
+    {id: 5, universe: "marvel", name: "Hulk"}
+], "universe"))
+
